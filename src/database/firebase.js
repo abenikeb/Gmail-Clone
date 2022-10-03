a1aaec3d5d45fd11a2bbd2b9650cdab86da25388
abenikeb <abenikeb79@gmail.com>
2022-10-03 11:58:53 +0300

    mgail clone

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,14 +1,24 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCgSlaWlwvpdlczOJ39TQxBTLCdd7KNQZM",
+//     authDomain: "gamil-clone-with-nathan.firebaseapp.com",
+//     projectId: "gamil-clone-with-nathan",
+//     storageBucket: "gamil-clone-with-nathan.appspot.com",
+//     messagingSenderId: "560695019982",
+//     appId: "1:560695019982:web:cc47eed71040d6f3cec39f"
+// };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgSlaWlwvpdlczOJ39TQxBTLCdd7KNQZM",
-    authDomain: "gamil-clone-with-nathan.firebaseapp.com",
-    projectId: "gamil-clone-with-nathan",
-    storageBucket: "gamil-clone-with-nathan.appspot.com",
-    messagingSenderId: "560695019982",
-    appId: "1:560695019982:web:cc47eed71040d6f3cec39f"
+  apiKey: "AIzaSyAXok43qPTDMM_cs54SdpnQScqbKPZaOzY",
+  authDomain: "clone-5438f.firebaseapp.com",
+  projectId: "clone-5438f",
+  storageBucket: "clone-5438f.appspot.com",
+  messagingSenderId: "585229062320",
+  appId: "1:585229062320:web:71491e11c655694f062b75",
+  measurementId: "G-BR7FL7FHYD",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -21,4 +31,4 @@ export const provider = new firebase.auth.GoogleAuthProvider();
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-export default db;  
+export default db;
